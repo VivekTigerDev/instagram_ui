5a7d90572caaaf2b6cd1134d3821dbcf8b0ba8dc
Vivek Yadav Developer <VivekTigerDev@users.noreply.github.com>
2021-03-30 16:39:56 +0530

    Some Changes

--- lib/ContentPage.dart
+++ lib/ContentPage.dart
@@ -1,16 +1,146 @@
 import 'package:flutter/material.dart';
 
 class ContentPage extends StatelessWidget {
+  final String userImage;
+  final String userName;
+  final String image;
+
+  const ContentPage({Key key, this.userImage, this.userName, this.image})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          Container(
-            child: Image.asset("images/14.png"),
-          )
-        ],
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 40.0,
+                      width: 40.0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(60.0),
+                        child: Image.asset(
+                          userImage,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    userName,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Icon(
+                Icons.more_horiz,
+                color: Colors.black54,
+              ),
+            ),
+          ],
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width,
+          child: Image.asset(
+            image,
+            filterQuality: FilterQuality.high,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 16.0,
+                  ),
+                  Icon(
+                    Icons.favorite_border_outlined,
+                    color: Colors.black54,
+                    size: 25.0,
+                  ),
+                  SizedBox(
+                    width: 16.0,
+                  ),
+                  Icon(
+                    Icons.chat_bubble_outline,
+                    color: Colors.black54,
+                    size: 25.0,
+                  ),
+                  const SizedBox(
+                    width: 16.0,
+                  ),
+                  Icon(
+                    Icons.send,
+                    color: Colors.black54,
+                    size: 25.0,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.bookmark_border,
+                color: Colors.black54,
+                size: 25.0,
+              ),
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+          child: Text(
+            "Liked by Naruto and others",
+            style: TextStyle(
+              color: Colors.black54,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+          child: Text(
+            "Sasuke Work Progress",
+            style: TextStyle(
+              color: Colors.black54,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+          child: Text(
+            "Naruto Comments.....",
+            style: TextStyle(
+              color: Colors.black54,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+          child: Text(
+            "10 Minutes Ago",
+            style: TextStyle(
+              color: Colors.black54,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_ui/ContentPage.dart';
 
 class Homescreen extends StatefulWidget {
   @override
@@ -39,7 +40,6 @@ class _HomescreenState extends State<Homescreen> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
-        
         leading: Icon(
           Icons.add_box_outlined,
           color: Colors.black54,
@@ -56,7 +56,7 @@ class _HomescreenState extends State<Homescreen> {
                     fontFamily: "Pacifico",
                     fontSize: 35.0),
               ),
-            )
+            ),
           ],
         ),
         actions: <Widget>[
@@ -68,38 +68,155 @@ class _HomescreenState extends State<Homescreen> {
         ],
       ),
 
-      // This Container Use For Story Content
-      body: Padding(
-        padding: const EdgeInsets.all(5.0),
-        child: Container(
-          height: 86.0,
-          color: Colors.white,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              firstlist("images/1.png", "Sasuke Uchiha"),
-              firstlist("images/2.png", "Jujustsu"),
-              firstlist("images/3.png", "Fairy Tail"),
-              firstlist("images/4.png", "Gara "),
-              firstlist("images/5.png", "Kakashi Senesi"),
-              firstlist("images/6.png", "Madara"),
-              firstlist("images/7.png", "Itachi Uchiha"),
-              firstlist("images/8.png", "Sakura"),
-              firstlist("images/9.png", "Kibo"),
-              firstlist("images/10.png", "Kibo Hakamaru"),
-              firstlist("images/11.png", "Shino"),
-              firstlist("images/12.png", "Sai"),
-              firstlist("images/13.png", "Hinata"),
-              firstlist("images/14.png", "Naruto"),
-              firstlist("images/15.png", "Lee"),
-              firstlist("images/16.png", "Pain"),
-              firstlist("images/17.png", "Tsunade"),
-              firstlist("images/18.png", "Iruka Senesi"),
-              firstlist("images/19.png", "Asuma Senesi"),
-              firstlist("images/20.png", "Shikamaru"),
-            ],
+      // This Container Use For Story List
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Container(
+              height: 102.0,
+              color: Colors.white,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  firstlist("images/1.png", "Sasuke Uchiha"),
+                  firstlist("images/2.png", "Jujustsu"),
+                  firstlist("images/3.png", "Fairy Tail"),
+                  firstlist("images/4.png", "Gara "),
+                  firstlist("images/5.png", "Kakashi Senesi"),
+                  firstlist("images/6.png", "Madara"),
+                  firstlist("images/7.png", "Itachi Uchiha"),
+                  firstlist("images/8.png", "Sakura"),
+                  firstlist("images/9.png", "Kibo"),
+                  firstlist("images/10.png", "Kibo Hakamaru"),
+                  firstlist("images/11.png", "Shino"),
+                  firstlist("images/12.png", "Sai"),
+                  firstlist("images/13.png", "Hinata"),
+                  firstlist("images/14.png", "Naruto"),
+                  firstlist("images/15.png", "Lee"),
+                  firstlist("images/16.png", "Pain"),
+                  firstlist("images/17.png", "Tsunade"),
+                  firstlist("images/18.png", "Iruka Senesi"),
+                  firstlist("images/19.png", "Asuma Senesi"),
+                  firstlist("images/20.png", "Shikamaru"),
+                ],
+              ),
+            ),
           ),
-        ),
+
+          //This Section Is For Content Page
+          Container(
+            height: 2.0,
+            color: Colors.white.withOpacity(0.1),
+          ),
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                ContentPage(
+                  userName: "Sasuke Uchiha",
+                  userImage: "images/1.png",
+                  image: "images/1.png",
+                ),
+                ContentPage(
+                  userName: "Kisame",
+                  userImage: "images/2.png",
+                  image: "images/2.png",
+                ),
+                ContentPage(
+                  userName: "Jujutsu",
+                  userImage: "images/3.png",
+                  image: "images/3.png",
+                ),
+                ContentPage(
+                  userName: "Gara",
+                  userImage: "images/4.png",
+                  image: "images/4.png",
+                ),
+                ContentPage(
+                  userName: "Kakashi Senesi",
+                  userImage: "images/5.png",
+                  image: "images/5.png",
+                ),
+                ContentPage(
+                  userName: "Madara",
+                  userImage: "images/6.png",
+                  image: "images/6.png",
+                ),
+                ContentPage(
+                  userName: "Itachi Uchiha",
+                  userImage: "images/7.png",
+                  image: "images/7.png",
+                ),
+                ContentPage(
+                  userName: "Sakura",
+                  userImage: "images/8.png",
+                  image: "images/8.png",
+                ),
+                ContentPage(
+                  userName: "Kibo",
+                  userImage: "images/9.png",
+                  image: "images/9.png",
+                ),
+                ContentPage(
+                  userName: "Kibo Hakamaru",
+                  userImage: "images/10.png",
+                  image: "images/10.png",
+                ),
+                ContentPage(
+                  userName: "Shino",
+                  userImage: "images/11.png",
+                  image: "images/11.png",
+                ),
+                ContentPage(
+                  userName: "Sai",
+                  userImage: "images/12.png",
+                  image: "images/12.png",
+                ),
+                ContentPage(
+                  userName: "Hinata",
+                  userImage: "images/13.png",
+                  image: "images/13.png",
+                ),
+                ContentPage(
+                  userName: "Naruto",
+                  userImage: "images/14.png",
+                  image: "images/14.png",
+                ),
+                ContentPage(
+                  userName: "Rock Lee",
+                  userImage: "images/15.png",
+                  image: "images/15.png",
+                ),
+                ContentPage(
+                  userName: "Pain",
+                  userImage: "images/16.png",
+                  image: "images/16.png",
+                ),
+                ContentPage(
+                  userName: "Tsunade",
+                  userImage: "images/17.png",
+                  image: "images/17.png",
+                ),
+                ContentPage(
+                  userName: "Iruka Senesi",
+                  userImage: "images/18.png",
+                  image: "images/18.png",
+                ),
+                ContentPage(
+                  userName: "Asuma Senesi",
+                  userImage: "images/19.png",
+                  image: "images/19.png",
+                ),
+                ContentPage(
+                  userName: "Shikamaru",
+                  userImage: "images/20.png",
+                  image: "images/20.png",
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
 
       // This Widgets Is Use For Making BottomAppBar Of Instagram
@@ -137,7 +254,7 @@ class _HomescreenState extends State<Homescreen> {
                   iconSize: 30.0,
                   color: Colors.black54,
                 ),
-               
+
                 // InkWell class Null safety. A rectangular/CircleAvatar area of
                 // a Material that responds to touch
                 InkWell(
